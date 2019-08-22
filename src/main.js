ec39e56f672bb69b9a31a57c6aa5d8310f5cca63
蔡木界 <1663744776@qq.com>
2019-08-22 16:15:39 +0800

    添加路由和创建路由对象

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import '@/styles/index.less'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router/router.js'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,22 @@
+// 引入路由模块
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Login from '@/views/login.vue'
+Vue.use(VueRouter)
+
+// 创建路由对象
+export default new VueRouter({
+  routes: [
+    {
+      name: 'default',
+      path: '/',
+      redirect: { name: 'login' }
+    },
+    {
+      name: 'logn',
+      path: '/login',
+      component: Login
+    }
+  ]
+})
